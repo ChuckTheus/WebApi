3822b9fbefeb00b6852b5f9356ad7217360c3329
Matheus <Matheus@DESKTOP-EL196IJ>
2024-09-17 21:35:00 -0300

    Add project files.

---
+++ ViewObjects/CandidatoVo.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.ViewObjects
+{
+    public class CandidatoVo
+    {
+        public string Nome { get; set; }
+        public string UrlLinkedin { get; set; }
+        public string UrlGitHub { get; set; }
+    }
+}
